6fa5d3294766f67f2fe3702f46ef0128e8ab6991
Fred Higgins <higgins.fred.s@gmail.com>
2022-09-17 17:40:27 -0700

    Add React Router to Index.js and set up first route in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,15 @@
 import '../App.css';
 import Home from './Home.js'
 import NavBar from './NavBar.js'
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header>
         <NavBar />
-      </header>
-      <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
